635feeecb30a13f1159dbf9dd4ea925dc7268ef1
marwan <marwan0320@live.nl>
2019-10-30 13:29:59 +0100

    Uitwerking van de exercise

---
+++ klas.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$stacks = array();
+echo "Wie zit er in de klas?\n";
+  $antwoord=readline();
+  $namen = explode(",", $antwoord);
+{
+    echo"> De studenten in de klas zijn:\n";
+}
+foreach ($namen as $value){
+echo $value .PHP_EOL ;
+
+}
